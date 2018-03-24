47f04fd68005bd710cf7e661ed2b56a40440c69e
RafaelAmaralSilva <rafaelamaralsilva01@gmail.com>
2018-03-24 15:37:56 -0300

    Correção no compose e renomeando classes

--- src/BuildingBlocks/Shuttle.Bus/RabbitMQConnection.cs
+++ src/BuildingBlocks/Shuttle.Bus/RabbitMQConnection.cs
@@ -1,5 +1,4 @@
-﻿using Newtonsoft.Json;
-using Polly;
+﻿using Polly;
 using Polly.Retry;
 using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
@@ -7,64 +6,9 @@ using RabbitMQ.Client.Exceptions;
 using System;
 using System.IO;
 using System.Net.Sockets;
-using System.Text;
 
 namespace Shuttle.Bus
 {
-    public class DiscoveryShuttle : IShuttle
-    {
-        private const string BROKER_NAME = "Cosmos";
-        private readonly RabbitMQConnection connection;
-
-        public DiscoveryShuttle(RabbitMQConnection connection)
-        {
-            this.connection = connection;
-        }
-
-        public void Publish<T>(T message)
-        {
-            if (!connection.IsConnected)
-            {
-                connection.TryConnect();
-            }
-
-            var policy = RetryPolicy
-                .Handle<BrokerUnreachableException>()
-                .Or<SocketException>()
-                .WaitAndRetry(
-                    5,
-                    retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)),
-                    (ex, time) =>
-                    {
-                        // _logger.LogWarning(ex.ToString());
-                    });
-
-            using (var channel = connection.CreateModel())
-            {
-                var eventName = message.GetType().Name;
-
-                channel.ExchangeDeclare(
-                    BROKER_NAME,
-                    type: "direct");
-
-                var @event = JsonConvert.SerializeObject(message);
-                var body = Encoding.UTF8.GetBytes(@event);
-
-                policy.Execute(() =>
-                {
-                    var properties = channel.CreateBasicProperties();
-                    properties.DeliveryMode = 2; // persistent
-
-                    channel.BasicPublish(exchange: BROKER_NAME,
-                                     routingKey: eventName,
-                                     mandatory: true,
-                                     basicProperties: properties,
-                                     body: body);
-                });
-            }
-        }
-    }
-
     public class RabbitMQConnection : IDisposable
     {
         private readonly IConnectionFactory connectionFactory;
